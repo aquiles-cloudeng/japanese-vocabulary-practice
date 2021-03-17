4ebefab620465b08f69266f61cd3612f42d37c81
aqcalvo <aqcalvo@serv.externos.prismamp.com>
2021-03-17 01:52:46 -0300

    updated to function

--- japanese-test.py
+++ japanese-test.py
@@ -1,14 +1,21 @@
 import random
 import sys
+def unidaduno():
+    listaunidaduno = {'わたし' : 'yo', 'わたしたち' : 'nosotros'}
+    keys = list(listaunidaduno.keys())
+    random.shuffle(keys)
+    x = True
+    for x in keys:
+        print("¿Que significa: " + x + "?")
+        res = input()
+        if res == listaunidaduno[x]:
+            print("Correcto!")
+        else:
+            print("Incorrecto, la respuesta correcta era: " + listaunidaduno[x])
 
-unidaduno = {'わたし' : 'yo', 'わたしたち' : 'nosotros'}
-keys = list(unidaduno.keys())
-random.shuffle(keys)
-x = True
-for x in keys:
-    print("¿Que significa: " + x + "?")
-    res = input()
-    if res == unidaduno[x]:
-        print("Correcto!")
-    else:
-        print("Incorrecto, la respuesta correcta era: " + unidaduno[x])
+print("Elegí un número de unidad para practicar")
+uni = int(input())
+if uni == 1:
+    unidaduno()
+else:
+    print("la unidad todavía no está programada!")
