5d4c5437f7538a35971f1c75461ce9111fc83d26
aqcalvo <aqcalvo@serv.externos.prismamp.com>
2021-03-18 19:17:56 -0300

    upgraded function to be only one

--- japanese-test.py
+++ japanese-test.py
@@ -1,29 +1,35 @@
 import random
 import sys
-def unidaduno():
-    listaunidaduno = {'わたし' : 'yo', 'わたしたち' : 'nosotros'}
-    keys = list(listaunidaduno.keys())
+
+listaunidad1 = {'わたし' : 'yo', 'わたしたち' : 'nosotros'}
+listaunidad2 = {'これ' : 'esto', 'それ' : 'ese', 'あれ' : 'aquello'}
+
+
+def ejercicio(uniraw):
+    unidad = eval("listaunidad" + uniraw)
+    keys = list(unidad.keys())
     random.shuffle(keys)
     x = True
     for x in keys:
         print("¿Que significa: " + x + "?")
         res = input()
-        if res == listaunidaduno[x]:
+        if res == unidad[x]:
             print("Correcto!")
         else:
-            print("Incorrecto, la respuesta correcta era: " + listaunidaduno[x])
+            print("Incorrecto, la respuesta correcta era: " + unidad[x]) #agregar una lista donde guarda los errores
+        #opcion de guardar el archivo al final
 
 def inicio():
     print("Elegí un número de unidad para practicar")
     uniraw = input()
     if uniraw.isdecimal():
-        uni = int(uniraw)
-        if uni == 1:
-            unidaduno()
-        else:
-            print("la unidad todavía no está programada!")
+        try:
+            ejercicio(uniraw)
+        except:
+            print("La unidad " + uniraw + " aun no está codeada")
     else:
         print("Por favor ingresar el número de unidad en decimal: (1, 2, 3, etc)")
         inicio()
 
 inicio()
+
